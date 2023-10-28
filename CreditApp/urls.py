f76f11e5c0f5cf7d84b0edee8ec65e8b8400e823
Muneeb Khan <muneebkhan@Muneebs-MacBook-Pro.local>
2023-10-28 17:13:38 +0100

    fixed mapping

--- CreditApp/urls.py
+++ CreditApp/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('credit/', include('credit_education.urls')),
+    path('', include('credit_education.urls')),
 
 ]
